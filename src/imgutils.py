e4f716034ae2aabbcc8f857e996f3965389e0636
humanova <craxes_humanova@hotmail.com>
2019-06-24 13:17:23 +0300

    fixed url2img func. not working on specific urls

--- src/imgutils.py
+++ src/imgutils.py
@@ -2,22 +2,25 @@ import os
 import glob
 import numpy as np
 from urllib.request import urlopen
+from urllib.parse import urlparse
 import cv2
 import base64
 
-
 def url2img(url, readFlag=cv2.IMREAD_COLOR):
     
-    suffix_list = ['jpg', 'gif', 'png', 'tif', 'svg']
-    file_name = url[url.rfind("/")+1:]
-    file_suffix = file_name.split('.')[1]
+    suffix_list = ['.jpg', '.gif', '.png', '.tif', '.svg']
+
+    path = urlparse(url)
+    p, file_suffix = os.path.splitext(path.path)
+    file_name = os.path.basename(p)
 
     if file_suffix in suffix_list:
         try:
             resp = urlopen(url)
             image = np.asarray(bytearray(resp.read()), dtype="uint8")
             image = cv2.imdecode(image, readFlag)
-        except:
+        except Exception as e:
+            print("Error while getting image from url", e)
             return False, None
         return True, image
 
--- wrapper/faceDet.py
+++ wrapper/faceDet.py
@@ -66,13 +66,19 @@ def decodeb64img(encoded_string, readFlag=cv2.IMREAD_COLOR):
 
 def url2img(url, readFlag=cv2.IMREAD_COLOR):
     
-    if isImgURL(url):
+    suffix_list = ['.jpg', '.gif', '.png', '.tif', '.svg']
+
+    path = urlparse(url)
+    p, file_suffix = os.path.splitext(path.path)
+    file_name = os.path.basename(p)
+
+    if file_suffix in suffix_list:
         try:
-            resp = urlopen(url, timeout=4.0)
+            resp = urlopen(url)
             image = np.asarray(bytearray(resp.read()), dtype="uint8")
             image = cv2.imdecode(image, readFlag)
         except Exception as e:
-            print(e)
+            print("Error while getting image from url", e)
             return False, None
         return True, image
 
